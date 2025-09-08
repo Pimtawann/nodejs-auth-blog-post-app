7e68e85edbd7217d9186dc5b2fa53c0e4fb81dd5
PTAE43 <kritsada1696@gmail.com>
2025-09-08 17:30:17 +0700

    feat: setup client

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = React.createContext();
 
@@ -9,24 +12,37 @@ function AuthProvider(props) {
     user: null,
   });
 
-  const login = () => {
+  const login = async (data) => {
     // 🐨 Todo: Exercise #4
     //  ให้เขียน Logic ของ Function `login` ตรงนี้
     //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+    const result = await axios.post("http://localhost:4000/auth/login", data);
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+
+    const userDataFromToken = jwtDecode(token);
+    setState({ ...state, user: userDataFromToken });
+    navigate("/");
   };
 
-  const register = () => {
+  const navigate = useNavigate();
+
+  const register = async (data) => {
     // 🐨 Todo: Exercise #2
     //  ให้เขียน Logic ของ Function `register` ตรงนี้
     //  Function register ทำหน้าที่สร้าง Request ไปที่ API POST /register
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+    await axios.post("http://localhost:4000/auth/register", data);
+    navigate("/login");
   };
 
   const logout = () => {
     // 🐨 Todo: Exercise #7
     //  ให้เขียน Logic ของ Function `logout` ตรงนี้
     //  Function logout ทำหน้าที่ในการลบ JWT Token ออกจาก Local Storage
+    localStorage.removeItem("token");
+    navigate("/login");
   };
 
   const isAuthenticated = Boolean(localStorage.getItem("token"));
