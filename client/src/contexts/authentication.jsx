edf54b11ce828c57053363cf9066d0abab008da2
Pimtawan Wonnawijit <pimtawan@MacBook-Air-khxng-Pimtawan.local>
2025-09-08 16:05:13 +0700

    feat: implement authentication API with login and registration endpoints

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = React.createContext();
 
@@ -8,19 +11,21 @@ function AuthProvider(props) {
     error: null,
     user: null,
   });
+  
+  const navigate = useNavigate();
 
-  const login = () => {
-    // 🐨 Todo: Exercise #4
-    //  ให้เขียน Logic ของ Function `login` ตรงนี้
-    //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
-    //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+  const login = async (data) => {
+    const result = await axios.post("http://localhost:4000/auth/login", data);
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+    const userDataFromToken = jwtDecode(token);
+    setState({ ...state, user: userDataFromToken });
+    navigate("/")
   };
 
-  const register = () => {
-    // 🐨 Todo: Exercise #2
-    //  ให้เขียน Logic ของ Function `register` ตรงนี้
-    //  Function register ทำหน้าที่สร้าง Request ไปที่ API POST /register
-    //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+  const register = async (data) => {
+    await axios.post("http://localhost:4000/auth/register", data);
+    navigate("/login");
   };
 
   const logout = () => {
