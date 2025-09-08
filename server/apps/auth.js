e2c21419340aeec2a415aed43a8c2aac98adc5cc
PTAE43 <kritsada1696@gmail.com>
2025-09-08 14:49:33 +0700

    feat: Ex.1 Register

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,9 +1,29 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    const collention = db.collection("users");
+    await collention.insertOne(user);
+
+    return res.json({
+        message: "User has been created successfully",
+    });
+});
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
