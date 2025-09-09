fa2a25287f8b9b4c7b83a4bb094a5857cb66946d
Jue Parinthon <jue.prt@hotmail.com>
2025-09-09 16:48:16 +0700

    feat: update axios version, add jwt-decode, and implement JWT authentication
    - Updated axios to version 1.11.0 and added jwt-decode as a dependency.
    - Integrated JWT authentication in the app by decoding the token on app load and managing user state.
    - Implemented a JWT interceptor to attach the token to requests and handle unauthorized responses by redirecting to the login page.
    - Added logout functionality in the HomePage component to clear the token and navigate to the login page.

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = React.createContext();
 
@@ -12,26 +14,49 @@ function AuthProvider(props) {
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const userData = jwtDecode(token);
+        setState({loading: false, error: null, user: userData });
+      } catch {
+        localStorage.removeItem("token");
+      }
+    }
+  }, []);
+
   const login = async (data) => {
-    const result = await axios.post("http://localhost:4000/auth/login", data);
-    const token = result.data.token;
-    localStorage.setItem("token", token);
-    const userDataFromToken = jwtDecade(token);
-    navigate("/");
-    };
+    try {
+      setState((prev) => ({ ...prev, loading: true, error: null }));
+      const result = await axios.post("http://localhost:4000/auth/login", data);
+      const token = result.data.token;
+      localStorage.setItem("token", token);
+      const userDataFromToken = jwtDecode(token);
+      navigate("/");
+
+    } catch (error) {
+      setState({
+        loading: false,
+        error: error.response?.data?.message || "Login Failed",
+        user: null,
+      });
+    }
+  };
 
   const register = async (data) => {
     await axios.post("http://localhost:4000/auth/register", data);
     navigate("/login");
-
   };
 
   const logout = () => {
     localStorage.removeItem("token");
+    setState({loading: false, error: null, user: null});
     navigate("/login");
   };
 
-  const isAuthenticated = Boolean(localStorage.getItem("token"));
+  const isAuthenticated = Boolean(state.user);
 
   return (
     <AuthContext.Provider
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import usePosts from "../hooks/usePosts";
 import getPublishedDate from "../utils/getPublishedDate";
 
+import { useAuth } from "../contexts/authentication";
+
 function HomePage() {
+  const { logout } = useAuth();
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
   const [status, setStatus] = useState("");
@@ -31,7 +34,7 @@ function HomePage() {
            // 🐨 Todo: Exercise #7
           //  นำ Function `logout` จาก AuthContext มา Execute ใน Prop `onClick`
         */}
-        <button>Logout</button>
+        <button onClick={logout}>Logout</button>
       </div>
       <div className="search-box-container">
         <div className="search-box">
